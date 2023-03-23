29ac0a4093cbe0330d7607a68b9c72c21ef48ab3
quojama <giantkoyama@gmail.com>
2023-03-24 03:46:19 +0900

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+import chromedriver_binary
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+load_dotenv()
+
+driver = webdriver.Chrome('chromedriver.exe')
+
+driver.get('https://diskunion.net/ec/ct/mypage')
+time.sleep(2)
+mail = driver.find_element(By.NAME, 'email_address')
+password = driver.find_element(By.NAME, 'password')
+mail.send_keys(os.environ['MAIL'])
+password.send_keys(os.environ['PASSWORD'])
+
+password.submit()
+
+time.sleep(1)
+
+driver.get('https://diskunion.net/ec/ct/fav_goods')
+
+time.sleep(1)
+
+tables = driver.find_elements(By.CLASS_NAME, 'tag-normal--blue')
+for table in tables:
+    elem = table.find_element(By.TAG_NAME, 'a')
+    href = elem.get_attribute("href")
+    print(href)
